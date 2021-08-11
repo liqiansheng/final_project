74e2b604908200a98ebb4096761ae3b8564c229c
liqiansheng <87025707+liqiansheng@users.noreply.github.com>
2021-08-11 21:06:39 +0800

    ranking part3 (icu info)
    also API key feature

--- app/covid_lookup.py
+++ app/covid_lookup.py
@@ -3,12 +3,15 @@ import json
 from pprint import pprint
 from getpass import getpass
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
+load_dotenv()
 
 user_statepool = []
 statepool = []
 api_key = os.getenv("USER_API_KEY")
+
+# List for ranking
 population_list = []
 casedensity_list = []
 
@@ -21,12 +24,19 @@ totalvacdistributed_list = []
 vacinitiatedratio_list = []
 vaccompletedratio_list = []
 
+totalicubed_list = []
+icubeduse_list = []
+icubedratio_list = []
+covidicu_list = []
+noncovidicu_list = []
 
-url = f"https://api.covidactnow.org/v2/states.json?apiKey=6a98cc92b3324c62aef26c681936f73c"
+
+url = f"https://api.covidactnow.org/v2/states.json?apiKey={api_key}"
 response = requests.get(url)
 parsed_response = json.loads(response.text)
+del parsed_response[25]
 
-# append states to the list
+# append states and other variables to the list
 for state in parsed_response:
     statepool.append(state["state"])
     population_list.append(state["population"])
@@ -41,8 +51,18 @@ for state in parsed_response:
     vacinitiatedratio_list.append(state["metrics"]["vaccinationsInitiatedRatio"])
     vaccompletedratio_list.append(state["metrics"]["vaccinationsCompletedRatio"])
 
+    totalicubed_list.append(state["actuals"]["icuBeds"]["capacity"])
+    icubeduse_list.append(state["actuals"]["icuBeds"]["currentUsageTotal"])
+    icubedratio_list.append(state["metrics"]["icuCapacityRatio"])
+    covidicu_list.append(state["metrics"]["icuHeadroomDetails"]["currentIcuCovid"])
+    noncovidicu_list.append(state["metrics"]["icuHeadroomDetails"]["currentIcuNonCovid"])
 
 
+# print(statepool[25])
+# print(statepool)
+# print(totalicubed_list)
+# print(icubeduse_list)
+# print(icubedratio_list)
 # sort list
 population_listsort = sorted(population_list,reverse = True)
 casedensity_listsort = sorted(casedensity_list,reverse = True)
@@ -55,6 +75,15 @@ totalnewdeath_listsort = sorted(totalnewdeath_list,reverse = True)
 totalvacdistributed_listsort = sorted(totalvacdistributed_list, reverse = True)
 vacinitiatedratio_listsort = sorted(vacinitiatedratio_list,reverse = True)
 vaccompletedratio_listsort = sorted(vaccompletedratio_list, reverse = True)
+
+totalicubed_listsort = sorted(totalicubed_list, reverse = True)
+icubeduse_listsort = sorted(icubeduse_list,reverse = True)
+icubedratio_listsort = sorted(icubedratio_list, reverse = True)
+covidicu_listsort = sorted(covidicu_list, reverse = True)
+noncovidicu_listsort = sorted(noncovidicu_list, reverse = True)
+
+
+
 
 # ask user for the states
 while True:
@@ -97,9 +126,14 @@ for selected_state in user_statepool:
   print("The vaccination completed ratio is:",parsed_response[state_order]["metrics"]["vaccinationsCompletedRatio"],"(Rank",vaccompletedratio_listsort.index(parsed_response[state_order]["metrics"]["vaccinationsCompletedRatio"])+1,"in the US)")
 
   print("----------------------ICU Information---------------------")
-  print(selected_state,"has",parsed_response[state_order]["actuals"]["icuBeds"]["capacity"],"total ICU beds")
-  print(selected_state,"has",parsed_response[state_order]["actuals"]["icuBeds"]["currentUsageTotal"],"ICU beds in current use")
-  print("The ratio of ICU beds currently in use is:",parsed_response[state_order]["metrics"]["icuCapacityRatio"])
-  print(selected_state,"has",parsed_response[state_order]["metrics"]["icuHeadroomDetails"]["currentIcuCovid"],"COVID patients in ICU")
-  print(selected_state,"has",parsed_response[state_order]["metrics"]["icuHeadroomDetails"]["currentIcuNonCovid"],"non-COVID patients in ICU")
+  print(selected_state,"has",parsed_response[state_order]["actuals"]["icuBeds"]["capacity"],"total ICU beds","(Rank",totalicubed_listsort.index(parsed_response[state_order]["actuals"]["icuBeds"]["capacity"])+1,"in the US)")
+
+  print(selected_state,"has",parsed_response[state_order]["actuals"]["icuBeds"]["currentUsageTotal"],"ICU beds in current use","(Rank",icubeduse_listsort.index(parsed_response[state_order]["actuals"]["icuBeds"]["currentUsageTotal"])+1,"in the US)")
+
+  print("The ratio of ICU beds currently in use is:",parsed_response[state_order]["metrics"]["icuCapacityRatio"],"(Rank",icubedratio_listsort.index(parsed_response[state_order]["metrics"]["icuCapacityRatio"])+1,"in the US)")
+
+  print(selected_state,"has",parsed_response[state_order]["metrics"]["icuHeadroomDetails"]["currentIcuCovid"],"COVID patients in ICU","(Rank",covidicu_listsort.index(parsed_response[state_order]["metrics"]["icuHeadroomDetails"]["currentIcuCovid"])+1,"in the US)")
+
+  print(selected_state,"has",parsed_response[state_order]["metrics"]["icuHeadroomDetails"]["currentIcuNonCovid"],"non-COVID patients in ICU","(Rank",noncovidicu_listsort.index(parsed_response[state_order]["metrics"]["icuHeadroomDetails"]["currentIcuNonCovid"])+1,"in the US)")
+
   print("-----------------------------------------------------------\n\n\n\n\n")
