2bff6dd6d04c128a787c6cecdec57db7ff976977
liqiansheng <87025707+liqiansheng@users.noreply.github.com>
2021-08-12 15:09:28 +0800

    Definition updated

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -15,28 +15,18 @@ def about():
     print("ABOUT...")
     return render_template("about.html")
 
-# web_app/routes/home_routes.py
-
-# ...
-
-@home_routes.route("/hello")
-def hello_world():
-    print("HELLO...", dict(request.args))
-    # NOTE: `request.args` is dict-like, so below we're using the dictionary's `get()` method,
-    # ... which will return None instead of throwing an error if key is not present
-    # ... see also: https://www.w3schools.com/python/ref_dictionary_get.asp
-    name = request.args.get("name") or "World"
-    message = f"Hello, {name}!"
-    # return message
-    return render_template("hello.html", message=message)
-
 @home_routes.route("/abbreviation")
 def abbreviation():
     print("ABBREVIATION...")
     return render_template("abbreviation.html")
 
-@home_routes.route("/data_def.html")
-def data_def():
-    print("DATA DEF...")
-    return render_template("data_def.html")
+@home_routes.route("/definition.html")
+def definition():
+    print("DEFINITION...")
+    return render_template("definition.html")
+
+@home_routes.route("/definitions")
+def definitions():
+    print("DEFINITIONS...")
+    return render_template("definitions.html")
 
